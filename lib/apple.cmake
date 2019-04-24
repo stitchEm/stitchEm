45302b258ebbf23a9f2d3f0a54b28606c15d6944
jerem <jeremy.tellaa@tanker.io>
2019-04-24 15:24:54 +0200

    Add homebrew for mac

--- lib/apple.cmake
+++ lib/apple.cmake
@@ -6,11 +6,15 @@ set(VS_LIB_SYSTEM_INCLUDES
 
 find_library(IO_KIT IOKit REQUIRED)
 
-if(APPLE_MACPORTS)
+if(APPLE_MACPORTS OR APPLE_BREW)
   find_package(Ceres REQUIRED)
   set(CERES_LIBS general ${CERES_LIBRARIES})
 
-  set(EIGEN3_INCLUDE_DIRS /opt/local/include/eigen3)
+  if(APPLE_MACPORTS)
+    set(EIGEN3_INCLUDE_DIRS /opt/local/include/eigen3)
+  else()
+    set(EIGEN3_INCLUDE_DIRS /usr/local/include/eigen3)
+  endif()
 else()
   find_library(CERES_OPTIMIZED_LIBRARY NAMES ceres ceres.1 PATHS ${CMAKE_EXTERNAL_DEPS}/lib/ceres)
   set(CERES_LIBS
