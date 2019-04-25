592dec6419c67d153406fd4db1cae8e9059fe34b
jerem <jeremy.tellaa@tanker.io>
2019-04-25 16:44:10 +0200

    remove APPLE with external_deps
    Now we can only use mac ports or homebrew

--- lib/apple.cmake
+++ lib/apple.cmake
@@ -6,18 +6,11 @@ set(VS_LIB_SYSTEM_INCLUDES
 
 find_library(IO_KIT IOKit REQUIRED)
 
-if(APPLE_MACPORTS OR APPLE_BREW)
-  find_package(Ceres REQUIRED)
-  set(CERES_LIBS general ${CERES_LIBRARIES})
+find_package(Ceres REQUIRED)
+set(CERES_LIBS general ${CERES_LIBRARIES})
 
-  if(APPLE_MACPORTS)
-    set(EIGEN3_INCLUDE_DIRS /opt/local/include/eigen3)
-  else()
-    set(EIGEN3_INCLUDE_DIRS /usr/local/include/eigen3)
-  endif()
+if(MACPORTS)
+  set(EIGEN3_INCLUDE_DIRS /opt/local/include/eigen3)
 else()
-  find_library(CERES_OPTIMIZED_LIBRARY NAMES ceres ceres.1 PATHS ${CMAKE_EXTERNAL_DEPS}/lib/ceres)
-  set(CERES_LIBS
-      general ${CERES_OPTIMIZED_LIBRARY})
-  set(EIGEN3_INCLUDE_DIRS ${CMAKE_EXTERNAL_DEPS}/lib/eigen)
+  set(EIGEN3_INCLUDE_DIRS /usr/local/include/eigen3)
 endif()
